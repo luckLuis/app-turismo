44f8c0a2d0b1f7be77f0e556fcdf780c599e9bb2
José Luis Colcha <jose.colcha@epn.edu.ec>
2022-07-16 23:27:08 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,9 @@
 
 export const environment = {
   production: false,
-  ApiKeyGoogleMaps: 'AIzaSyD6JmoXo13d_mjPw5eQwRDlCTug14Akhbk',
+  ApiKeyGoogleMaps: '',
 
   firebase: {
-    apiKey: 'AIzaSyBqiDp30J9pPnWCSH4kSXFRKpcoMkvqd8E',
-    authDomain: 'examen-ib-bb884.firebaseapp.com',
-    projectId: 'examen-ib-bb884',
-    storageBucket: 'examen-ib-bb884.appspot.com',
-    messagingSenderId: '214531438662',
-    appId: '1:214531438662:web:0ea9e61ac8a5c6dea63247',
   },
 };
 /*
