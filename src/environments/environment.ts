f170562efc6db4f496567d287cc9aac993aee37f
luckLuis <58127103+luckLuis@users.noreply.github.com>
2022-07-16 11:22:25 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'app-turismo-69e7a',
-    appId: '1:574926027739:web:85e163d24c456f52b3c5fe',
-    storageBucket: 'app-turismo-69e7a.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyCJzCgzuKnuis2NTiJN_DDD6wEeqF0c-_s',
-    authDomain: 'app-turismo-69e7a.firebaseapp.com',
-    messagingSenderId: '574926027739',
-    measurementId: 'G-YK9FLVD7NY',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
   production: false
 };
